4c8caea0319bc8fc92ec6887d6a9ff846419495a
itsankitkgupta <ankitgupta132962@gmail.com>
2024-05-22 11:38:31 +0530

    second debug

--- controller/user/userSignIn.js
+++ controller/user/userSignIn.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const userModel = require('../../models/userModel')
 const jwt = require('jsonwebtoken');
+TOKEN_SECRET_KEY = "mynameisAnthonyGonzalves"
 
 async function userSignInController(req,res){
     try{
@@ -28,7 +29,7 @@ async function userSignInController(req,res){
             _id : user._id,
             email : user.email,
         }
-        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 8 });
+        const token = await jwt.sign(tokenData,TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 8 });
 
         const tokenOption = {
             httpOnly : true,
