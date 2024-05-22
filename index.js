c47ed6d4f7e2f66e52f46e78cdbe7c7f07ed64d2
itsankitkgupta <ankitgupta132962@gmail.com>
2024-05-22 18:48:34 +0530

    final commit in cors

--- index.js
+++ index.js
@@ -7,32 +7,32 @@ const router = require('./routes')
 
 
 const app = express()
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-//   });
-// app.use(cors({
-//     origin : "http://localhost:3000/",
-//     // optionsSuccessStatus: 200,
-//     credentials : true
-// }))
-// app.use(cors({
-//     origin: '*',
-//     credentials: true
-//   }));
-const corsOptions = {
-    origin: (origin, callback) => {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin || origin.startsWith('http://localhost')) {
-        callback(null, true); // Allow the request
-      } else {
-        callback(new Error('Not allowed by CORS')); // Block the request
-      }
-    },
-    credentials: true, // Allow credentials
-  };
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+app.use(cors({
+    origin : "http://localhost:3000/",
+    // optionsSuccessStatus: 200,
+    credentials : true
+}))
+app.use(cors({
+    origin: '*',
+    credentials: true
+  }));
+// const corsOptions = {
+//     origin: (origin, callback) => {
+//       // Allow requests with no origin (like mobile apps or curl requests)
+//       if (!origin || origin.startsWith('http://localhost')) {
+//         callback(null, true); // Allow the request
+//       } else {
+//         callback(new Error('Not allowed by CORS')); // Block the request
+//       }
+//     },
+//     credentials: true, // Allow credentials
+//   };
   
 app.use(cors(corsOptions));
 app.use(express.json())
