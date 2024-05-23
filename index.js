4b1bc09252e0f82de95f15548f91932fc31aa96b
itsankitkgupta <ankitgupta132962@gmail.com>
2024-05-23 10:53:47 +0530

    2nd day of fixing cors

--- index.js
+++ index.js
@@ -5,36 +5,27 @@ require('dotenv').config("./")
 const connectDB = require('./config/db')
 const router = require('./routes')
 
+const bodyParser = require('body-parser');
 
 const app = express()
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
-// app.use(cors({
-//     origin : "http://localhost:3000/",
-//     // optionsSuccessStatus: 200,
-//     credentials : true
-// }))
-app.use(cors({
-    origin: '*',
-    credentials: true
-  }));
-// const corsOptions = {
-//     origin: (origin, callback) => {
-//       // Allow requests with no origin (like mobile apps or curl requests)
-//       if (!origin || origin.startsWith('http://localhost')) {
-//         callback(null, true); // Allow the request
-//       } else {
-//         callback(new Error('Not allowed by CORS')); // Block the request
-//       }
-//     },
-//     credentials: true, // Allow credentials
-//   };
-  
-// app.use(cors(corsOptions));
+app.use(bodyParser.json({ limit: '50mb' }));
+
+const allowedOrigins = ['http://localhost:3000', 'https://your-frontend-domain.netlify.app'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use(cookieParser())
 
